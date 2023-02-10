18850973bc2ff6632f92ae62637db64c3c8aac94
odbayar <odbayardorjsuren8@gmail.com>
2023-02-10 18:17:04 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MiniDrawer from "./components/Category/Drawer";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <MiniDrawer />
+      {/* <box>box</box> */}
+    </>
+  );
+}
+
+export default App;
