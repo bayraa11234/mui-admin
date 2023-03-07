60bc1adaac136d038d3ba4658be2df3efc1ff2d2
odbayar <odbayardorjsuren8@gmail.com>
2023-03-07 18:55:14 +0800

    update

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -1,5 +1,9 @@
 import { Typography } from "@mui/material";
 
 export const HomeScreen = () => {
-  return <></>;
+  return (
+    <>
+      <Typography variant="h1">MUI ADMIN</Typography>
+    </>
+  );
 };
